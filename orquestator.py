27fa55b2dab7ad7bb6906eb3a4f2e653a44f5e52
Marcel Magrane <marcel.magrane@urv.cat>
2019-04-23 04:05:50 +0200

    master: tot fet

--- orquestator.py
+++ orquestator.py
@@ -1,8 +1,5 @@
-'''
-Created on 21 abr. 2019
+import sys
 
-@author: Marcel
-'''
 from cos_backend import COS_Backend
 from ibm_cf_connector import CloudFunctions
 import yaml
@@ -12,14 +9,55 @@ if __name__ == '__main__':
         res = yaml.safe_load(config_file)
 
     ibm_cos = res['ibm_cos']
-    ibm_functions = res['ibm_cf']
-    
+    ibm_cf = res['ibm_cf']
     cos = COS_Backend(ibm_cos)
-    #f = open('test.txt', 'r')
-    #cos.put_object('sdprac1', 'test.txt', f.read())
+    cloud = CloudFunctions(ibm_cf)
     
-    a=cos.get_object('sdprac1', 'test.txt', extra_get_args={'Range': 'bytes=0-100'})
-    string = a.decode('utf-8')
-    print(string)
-    b=cos.head_object('sdprac1', 'test.txt')
-    print(b['content-length'])
+    fileread=sys.argv[1]
+    partitions=int(sys.argv[2])
+    
+    #Add required files to the cloud
+    f = open(fileread, 'r')
+    cos.put_object('sdprac1', fileread, f.read())
+    
+    f = open('wordcount.zip', 'rb')
+    cloud.create_action('wordcount', f.read())
+    
+    #f = open('reducer.zip', 'rb')
+    #cloud.create_action('reduce', f.read)
+    
+    #f = open('countwords.zip', 'rb')
+    #cloud.create_action('countwords', f.read)
+    
+    #Create partitions of the data file to feed the different workers
+    filesize=int(cos.head_object('sdprac1', fileread)['content-length'])
+    print(filesize)
+    
+    partitionsize = int(round(filesize / partitions))
+    minimum = 0
+    maximum = partitionsize
+    x = 1
+    
+    #Initialize action wordcount
+    while x <= partitions:
+        a=cos.get_object('sdprac1', fileread, extra_get_args={'Range': 'bytes={0}-{1}'.format(minimum, maximum)})
+        text = a.decode('utf-8')
+        #print(text)
+        print('-------------------------------------------------------------------------------------')
+        cloud.invoke('wordcount', {"credentials":res['ibm_cos'], "text":text, "order":x})
+        minimum = maximum + 1
+        maximum = maximum + partitionsize
+        x += 1
+        if x == partitions or maximum > filesize:
+            maximum = filesize
+    
+    #Wait for wordcount
+    print("Waiting for the {0} wordcounts to finish:".format(partitions))
+    end = len(cos.list_objects('sdprac1', 'wc'))
+    while end != partitions:
+        end = len(cos.list_objects('sdprac1', 'wc'))
+        print("Not yet...")
+        
+    
+    
+    
